80673eecc257c3f1e8f61695c61e407d5c674b7c
Rio Prayoga <yoprayogap@gmail.com>
2022-11-16 12:46:27 +0700

    fetching data dari api

--- js/script.js
+++ js/script.js
@@ -9,16 +9,23 @@ if (window.location.pathname === "/sign-in.html") {
 
   formLogin.addEventListener("submit", (event) => {
     event.preventDefault();
+
+    //!get value dari input
     const email = event.target.email.value;
     const password = event.target.password.value;
 
+    //!cek apakah email dan password sesuai
     if (email === "admin@gmail.com" && password === "admin123") {
       alertSuccess.style.display = "block";
+
+      //!setelah 2 detik di redirect ke halaman home
       setTimeout(() => {
         window.location.replace("/home.html");
       }, 2000);
     } else {
       alertInvalid.style.display = "block";
+
+      //!setelah 5 detik alert di hilangkan
       setTimeout(() => {
         alertInvalid.style.display = "none";
       }, 5000);
@@ -29,10 +36,13 @@ if (window.location.pathname === "/sign-in.html") {
   });
 
   btnEye.addEventListener("click", (event) => {
+    //! cek type dari input
     if (inputPass.getAttribute("type") == "password") {
       inputPass.setAttribute("type", "text");
 
+      //! ubah feather icon
       const newIcon = feather.icons["eye"].toSvg();
+      //! timpah element icon
       wrapGPass.children[1].innerHTML = newIcon;
     } else {
       inputPass.setAttribute("type", "password");
@@ -97,12 +107,15 @@ if (window.location.pathname === "/sign-in.html") {
   const containerListUpcoming = document.getElementById(
     "container-list-upcoming"
   );
+  //! mapping data
   const movieNowShowing = dataMovie.map((movie) => {
     const { title, genre, img } = movie;
     return `<div class="card-movies" id="card-movies">
                     <img src="${img}" alt="">
                     <div class="detail-hover" id="detail-hover">
-                        <h3>${title}</h3>
+                 <div class="wrap-title">
+                 ${title}
+                 </div>
                         <p>${genre.join(", ")}</p>
                         <a href="movie-details.html">
                             <div>Details</div>
@@ -112,19 +125,47 @@ if (window.location.pathname === "/sign-in.html") {
   });
   containerList.innerHTML = movieNowShowing.join(" ");
 
-  const movieUpcoming = dataMovie.map((movie) => {
-    const { title, genre, img } = movie;
+  // const movieUpcoming = dataMovie.map((movie) => {
+  //   const { title, genre, img } = movie;
 
-    return `
-        <div class="card-movies">
-            <img src="${img}" alt="">
-                <h3>${title}</h3>
-                <p>${genre.join(", ")}</p>
-                <a href="movie-details.html">
-                    <div>Details</div>
-                </a>
-        </div>`;
-  });
+  //   return `
+  //       <div class="card-movies">
+  //           <img src="${img}" alt="">
+  //               <h3>${title}</h3>
+  //               <p>${genre.join(", ")}</p>
+  //               <a href="movie-details.html">
+  //                   <div>Details</div>
+  //               </a>
+  //       </div>`;
+  // });
 
-  containerListUpcoming.innerHTML = movieUpcoming.join(" ");
+  const getMovie = async () => {
+    const req = await fetch(
+      "https://www.omdbapi.com/?apikey=1e23b4b9&s=marvel"
+    );
+    const res = await req.json();
+
+    //!destruct properti Search
+    const { Search } = res;
+
+    //!Maping data
+    const template = Search.map((data) => {
+      const { Title, Poster, Year } = data;
+
+      return `<div class="card-movies">
+             <img src="${Poster}" alt="">
+                 <div class="wrap-title">
+                 ${Title}
+                 </div>
+                 <p>${Year}</p>
+                 <a href="movie-details.html">
+                     <div>Details</div>
+                 </a>
+         </div>`;
+    });
+
+    containerListUpcoming.innerHTML = template.join("");
+  };
+
+  getMovie();
 }
